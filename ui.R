e6b3d72726da738cf6eba643e507ae4510fb26ab
tanvird3 <tanvir.d3@gmail.com>
2021-05-09 22:52:46 +0600

    Design Modification

--- ui.R
+++ ui.R
@@ -1,62 +1,70 @@
-shinyUI(fluidPage (
-  theme = shinytheme("superhero"),
-  headerPanel("COVID-19 Data Visualizer"),
-  sidebarPanel(
-    width = 2,
-    selectInput(
-      "countries",
-      label = "Select Countries",
-      choices =
-        country_list,
-      selected = c(country_list[which(country_list == "Malaysia")], country_list[which(country_list ==
-                                                                                      "Nepal")], country_list[which(country_list == "Pakistan")]),
-      multiple = T
+shinyUI(
+  navbarPage(
+    "COVID-19 Data Visualizer",
+    tags$style(type = 'text/css', '.navbar {#font-family: Arial; font-size: 20px; }'), 
+    selected = "Global Status",
+    collapsible = TRUE,
+    inverse = TRUE,
+    theme = shinytheme("superhero"),
+    tabPanel(
+      "Global Status",
+      width = "100%",
+      div(plotlyOutput("figG"), align = "center"),
+      br(),
+      div(plotlyOutput("global_time"), align = "center"),
+      br(),
+      div(plotlyOutput("global_cfr"), align = "center"),
+      br(),
+      div(plotlyOutput("global_p"), align = "center"),
+      br(),
+      div(plotlyOutput("global_recov_dead"), align = "center")
     ),
-    selectInput(
-      "Scale",
-      label = "Comparative Charts' Scale",
-      choices =
-        c("Linear", "Log"),
-      selected = "Linear",
-      multiple = F
-    ),
-    submitButton(text = "View")
-  ),
-  mainPanel (
-    h1(""),
-    tabsetPanel(
-      tabPanel(
-        "Global Status",
-        plotlyOutput("figG"),
-        br(),
-        plotlyOutput("global_time"),
-        br(),
-        plotlyOutput("global_cfr"),
-        br(),
-        plotlyOutput("global_p"),
-        br(),
-        plotlyOutput("global_recov_dead")
-      ),
-      tabPanel(
-        "Comparative Charts",
-        plotlyOutput("fig_confirm"),
-        br(),
-        plotlyOutput("fig_dead"),
-        br(),
-        plotlyOutput("fig_recov")
-      ),
-      tabPanel(
-        "Metrics",
-        plotlyOutput("fig_confirm_S"),
-        br(),
-        plotlyOutput("fig_confirm_D"),
-        br(),
-        plotlyOutput("fig_Ratio"),
-        br(),
-        plotlyOutput("fig_cfr_print")
-      ),
-      tabPanel("Growth",
-        plotlyOutput("fig_cp_print"))
-    )
+    tabPanel("Analytics",
+             fluidPage(
+               sidebarPanel(
+                 width = 2,
+                 selectInput(
+                   "countries",
+                   label = "Select Countries",
+                   choices =
+                     country_list,
+                   selected = c(country_list[which(country_list == "Malaysia")], country_list[which(country_list ==
+                                                                                                      "Nepal")], country_list[which(country_list == "Pakistan")]),
+                   multiple = T
+                 ),
+                 selectInput(
+                   "Scale",
+                   label = "Comparative Charts' Scale",
+                   choices =
+                     c("Linear", "Log"),
+                   selected = "Linear",
+                   multiple = F
+                 ),
+                 submitButton(text = "View")
+               ),
+               mainPanel (h1(""),
+                          tabsetPanel(
+                            tabPanel(
+                              "Comparative Charts",
+                              div(plotlyOutput("fig_confirm"), align = "center"),
+                              br(),
+                              div(plotlyOutput("fig_dead"), align = "center"),
+                              br(),
+                              div(plotlyOutput("fig_recov"), align = "center")
+                            ),
+                            tabPanel(
+                              "Metrics",
+                              div(plotlyOutput("fig_confirm_S"), align = "center"),
+                              br(),
+                              div(plotlyOutput("fig_confirm_D"), align = "center"),
+                              br(),
+                              div(plotlyOutput("fig_Ratio"), align = "center"),
+                              br(),
+                              div(plotlyOutput("fig_cfr_print"), align = "center")
+                            ),
+                            tabPanel("Growth",
+                                     div(plotlyOutput("fig_cp_print"), align = "center"))
+                          ))
+             ))
   )
-))
+)
